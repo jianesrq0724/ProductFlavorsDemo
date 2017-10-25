b2088fe1b512274574d68b3063731d0c5743a8b4
shenruiqin <shenruiqin@ecaray.com>
2017-10-25 19:59:15 +0800

    优化后

--- app/src/main/java/com/ruiqin/productflavorsdemo/TestBaseActivity.java
+++ app/src/main/java/com/ruiqin/productflavorsdemo/TestBaseActivity.java
@@ -2,9 +2,6 @@ package com.ruiqin.productflavorsdemo;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -14,13 +11,7 @@ public class TestBaseActivity extends AppCompatActivity {
 
     public static void startActivity(Context context) {
         Intent intent = new Intent();
-        intent.setAction("TestBaseActivity");
-        intent.setData(Uri.parse("content://main"));
-        PackageManager packageManager = context.getPackageManager();
-        ResolveInfo resolveInfo = packageManager.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
-        if (resolveInfo == null) {
-            intent.setData(Uri.parse("content://" + BuildConfig.FLAVOR));
-        }
+        intent.setAction(BuildConfig.APPLICATION_ID + ".TestBaseActivity");
         context.startActivity(intent);
     }
 
